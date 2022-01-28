aee06e383bac72fe8d87a325695f7d008919d74c
godwin-nj <amadigodwin7@gmail.com>
2022-01-28 15:16:48 +0100

    expense form functionality

--- src/expenseManager/expenseForm.js
+++ src/expenseManager/expenseForm.js
@@ -3,44 +3,63 @@ import React,{useState} from 'react'
 import ExpenseData from './displayExpenseData'
 import { Navigate,useLocation,Link,useNavigate, } from "react-router-dom";
 import './expenseform.css'
+import axios from "axios";
 
 
 const ExpenseForm = () => {
     let navigate = useNavigate();
     // const [date, setDate] = useState('')
     // const [description, setDescription] = useState('')
-    const [category, setCategory] = useState({value :'feeding'})
+    // commented this 
+    const [category, setCategory] = useState({expenseType :''})
     // const [amount, setAmount] = useState('')
 
     const[form, setForm] = useState({
-        description : "",
+        title : "",
         date : "",
-        amount : ""
+        amount : "",
+        // expenseType :""
     })
 
-    const [user,setUser] = useState([])
+    const [user,setUser] = useState({})
 
 
     function expenseHandleChange(e){
         const name = e.target.name
         const value = e.target.value
         setForm({...form, [name] : value})
-
+        // setCategory({expenseType : e.target.value})
     }
 
    function dropdownChanged(e){
-    setCategory({value : e.target.value})
+    setCategory({expenseType : e.target.value})
     }
   
-    function expenseHandleSubmit(e){
+    const expenseHandleSubmit = async (e) =>{
         e.preventDefault()
-        if(form.description && form.date && form.amount && category.value){
-            const expenseDispatch = 
-            {...form,...category, timeStamp : new Date().getTime().toString()}
-            setUser([...user,expenseDispatch])  
-            setForm({description : "", date : "",amount : ""})
-            setCategory({value:""})
+        // give conditions if required data is not provided 
+        if(!(form.title && form.date && form.amount && category.expenseType)){
+            console.log('provide data')
+            return
         }
+        // const expenseDispatch = {...form,...category}
+        setUser({...form,...category})
+      
+        const data = await axios.post('/api/v1/expense',user)
+        if(!data){
+            console.log('error getting expense data')
+            return
+        }
+        setUser(data)
+        setForm({title : "", date : "",amount : ""})
+        setCategory({expenseType:""})
+        // if(form.title && form.date && form.amount && category.expenseType){
+        //     const expenseDispatch = 
+        //     {...form,...category, timeStamp : new Date().getTime().toString()}
+        //     setUser([...user,expenseDispatch])  
+        //     setForm({title : "", date : "",amount : ""})
+        //     setCategory({value:""})
+        // }
     //    return alert('provide information')
         
     }
@@ -56,12 +75,12 @@ const ExpenseForm = () => {
             <form className='expenseForm' onSubmit={expenseHandleSubmit}>
                 {/* description  */}
                 <div className='inputSingleContainer' style={{marginRight: '41px'}}>
-                    <label className='expenseLabel' htmlFor='description'>Description</label>
+                    <label className='expenseLabel' htmlFor='title'>Description</label>
                     <input 
                         type="text"
-                        name="description"
-                        id="description"
-                        value={form.description}
+                        name="title"
+                        id="title"
+                        value={form.title}
                         onChange={expenseHandleChange}
                         className='expenseInput'
                     />
@@ -82,15 +101,15 @@ const ExpenseForm = () => {
                 {/* category  */}
                 <div className='inputSingleContainer' style={{marginRight: '47px'}}>
                 <label className='expenseLabel' htmlFor='category' >Expense Type</label>
-                    <select value={category.value} onChange={dropdownChanged} style={{padding:'5px'}}>
-                        <option value="office Supplies" >Office Supplies</option>
-                        <option value="training" >Education & Training</option>
+                    <select value={category.expenseType} onChange={dropdownChanged} style={{padding:'5px'}}>
+                   
+                        <option value="office supplies" >office supplies</option>
+                        <option value="Education|training" >Education|training</option>
                         <option value="Feeding" >Feeding</option>
                         <option value="Housing">Housing</option>    
                         <option value="transport">transport</option>    
+                     
                     </select>
-                    {/* <input type="" id="category"/> */}
-                    {/* <input type="" value="submit"/> */}
                 </div>
 
                 {/* amount  */}
@@ -111,7 +130,7 @@ const ExpenseForm = () => {
                
             </form>
 
-            < ExpenseData user={user}/>
+            {/* < ExpenseData user={user}/> */}
             <button className='btnLogOut' onClick={() => logOut()}>Logout</button>
         </div>
     )
